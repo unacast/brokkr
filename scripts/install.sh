85d6492655bcc0c0abfba7c8b9308c68f2af8cdc
Ole Christian Langfjæran <judoole@gmail.com>
2020-03-17 23:20:33 +0100

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p brokkr
+curl --fail -s "https://raw.githubusercontent.com/judoole/brokkr/master/brokkr/brokkr.mk" -o brokkr/brokkr.mk
+
+if [ ! -f "Makefile" ]; then
+	echo '.SILENT: ;' > Makefile
+	echo 'BROKKR_PLUGINS = help/help-master.mk' >> Makefile
+	echo '.DEFAULT_GOAL := help' >> Makefile
+	echo -e '\n-include ./brokkr/brokkr.mk' >> Makefile
+elif [[ ! `grep 'include ./brokkr/brokkr.mk' Makefile` ]]; then
+	echo -e '\n-include ./brokkr/brokkr.mk' >> Makefile
+fi
