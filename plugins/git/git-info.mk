35ed0badc45ae9601f9b91851c56cb3b5a6be73b
Ole Christian Langfjæran <judoole@gmail.com>
2020-04-02 21:32:44 +0200

    Add a plugin for git and build info

---
+++ plugins/git/git-info.mk
@@ -0,0 +1,18 @@
+_GIT_SHA1=$(shell git rev-parse HEAD)
+_GIT_SHA1_SHORT=$(shell git rev-parse --short HEAD)
+_GIT_BRANCH=$(shell git rev-parse --abbrev-ref HEAD)
+_GIT_AUTHOR=$(shell git show -s --format='%ae')
+
+
+.PHONY: _git.buildinfo
+_git.buildinfo:
+	$(if $(value BUILD_INFO_PATH),, $(error BUILD_INFO_PATH environment variable is not set.\
+		Add this to your Makefile.))
+	echo [git] > $(BUILD_INFO_PATH)
+	echo sha1=$(GIT_SHA1) >> $(BUILD_INFO_PATH)
+	echo sha1_short=$(GIT_SHA1_SHORT) >> $(BUILD_INFO_PATH)
+	echo branch=$(GIT_BRANCH) >> $(BUILD_INFO_PATH)
+	echo author=$(GIT_AUTHOR) >> $(BUILD_INFO_PATH)
+	echo [build] >> $(BUILD_INFO_PATH)
+	echo deployed_by=$(USER) >> $(BUILD_INFO_PATH)
+	echo unix_timestamp=`date +%s` >> $(BUILD_INFO_PATH)
