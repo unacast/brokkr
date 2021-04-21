b2c389a2e1d5dc529608695e98b869d7951a6698
Torbjørn Vatn <torbjorn.vatn@unacast.com>
2021-04-21 15:36:12 +0200

    Update the github plugin with an automatic gh run watch

--- plugins/github/github-deploy.mk
+++ plugins/github/github-deploy.mk
@@ -9,15 +9,16 @@
 #   $(call, some-env, some-task)
 AUTO_MERGE=true
 define deploy
-	echo Checking diff local against remote
+	@echo Checking diff local against remote
 	# Get latest, so we can diff if there are changes
-	git fetch
 	# Check if there is staged code or changes. Exit if so
-	if git fetch && git diff @{push} --shortstat --exit-code; then \
+	@if git fetch && git diff @{push} --shortstat --exit-code > /dev/null 2>&1; then \
 		gh api repos/:owner/:repo/deployments -H "Accept: application/vnd.github.ant-man-preview+json" \
 	               --method POST -F ref=":branch" -F environment="$(strip $1)" -F auto_merge=${AUTO_MERGE} \
          	      -F task="$(strip $(if $2, $2, deploy))"; \
+        sleep 5; \
+		gh run list --limit 1 | grep -v "completed" | rev | cut -f1 | rev | xargs gh run watch; \
 	else \
-		echo "There is a git diff ⤴️, exiting..."; \
+		echo "There is a git diff ⤴️ or no remote branch, exiting..."; \
 	fi;
 endef
