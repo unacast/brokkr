88a9f1140f81da73f63f5231024b018eebc90898
Omer Haroon <omer@Omers-MBP.uc>
2022-03-17 09:28:20 +0100

    replace spaces

--- plugins/airflow/airflow.mk
+++ plugins/airflow/airflow.mk
@@ -17,96 +17,96 @@ AIRFLOW_REQUIREMENTS_EXTRA_TXT ?= requirements.extra.txt
 
 .PHONY: airflow.start
 airflow.start: $(AIRFLOW_BUILD_SENTINEL) $(AIRFLOW_VARIABLES_SENTINEL) ## Start Airflow server
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) up -d db
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) up -d webserver
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) up -d scheduler
-    echo "Now running at http://localhost:$(shell grep "AIRFLOW_WEBSERVER_PORT" .env | cut -d "=" -f2)/"
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) up -d db
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) up -d webserver
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) up -d scheduler
+	echo "Now running at http://localhost:$(shell grep "AIRFLOW_WEBSERVER_PORT" .env | cut -d "=" -f2)/"
 
 .PHONY: airflow.stop
 airflow.stop: $(AIRFLOW_INIT_CHECK_SENTINEL) ## Stop running Airflow server
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) stop
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) stop
 
 .PHONY: airflow.logs
 airflow.logs: $(AIRFLOW_INIT_CHECK_SENTINEL) ## Tail the local logs
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) logs --follow
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) logs --follow
 
 .PHONY: airflow.test
 airflow.test: $(AIRFLOW_BUILD_SENTINEL) $(AIRFLOW_INIT_CHECK_SENTINEL) ## Run the tests found in /test
-    set +e;docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) run --rm  test pytest --html=/code/output/report.html --junitxml=/code/output/junit.xml -rA /code/tests; \
-    open $${PWD}/output/report.html
+	set +e;docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) run --rm  test pytest --html=/code/output/report.html --junitxml=/code/output/junit.xml -rA /code/tests; \
+	open $${PWD}/output/report.html
 
 .PHONY: airflow.flake8
 airflow.flake8:$(AIRFLOW_BUILD_SENTINEL) $(AIRFLOW_INIT_CHECK_SENTINEL) ## Run the flake8 agains dags folder
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) run -v ${PWD}/.flake8:/code/.flake8 --rm test flake8 /code/dags
-    @echo Flake 8 OK!s
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) run -v ${PWD}/.flake8:/code/.flake8 --rm test flake8 /code/dags
+	@echo Flake 8 OK!s
 
 .PHONY: airflow.clean
 airflow.clean: ## Removes .airflow folder and docker containers
-    echo Removing containers
-    docker ps -a --filter "name=$(COMPOSE_PROJECT_NAME)_" --format "{{.Names}}" | xargs docker rm -f
-    echo Removing data volume
-    docker volume ls --format "{{.Name}}" --filter "name=$(COMPOSE_PROJECT_NAME)_data-volume" | xargs docker volume rm
-    $(info Deleting $(AIRFLOW_WORKFOLDER))
-    rm -rf $(AIRFLOW_WORKFOLDER)
+	echo Removing containers
+	docker ps -a --filter "name=$(COMPOSE_PROJECT_NAME)_" --format "{{.Names}}" | xargs docker rm -f
+	echo Removing data volume
+	docker volume ls --format "{{.Name}}" --filter "name=$(COMPOSE_PROJECT_NAME)_data-volume" | xargs docker volume rm
+	$(info Deleting $(AIRFLOW_WORKFOLDER))
+	rm -rf $(AIRFLOW_WORKFOLDER)
 
 .PHONY: airflow.error
 airflow.error: $(AIRFLOW_INIT_CHECK_SENTINEL) ## List all dags, and filter errors
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) run webserver airflow list_dags | grep -B5000 "DAGS"
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) run webserver airflow list_dags | grep -B5000 "DAGS"
 
 .PHONY: airflow.build
 airflow.build: $(AIRFLOW_INIT_CHECK_SENTINEL) ## Rebuild docker images with --no-cache. Useful for debugging
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) build --no-cache --force-rm --pull
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) build --no-cache --force-rm --pull
 
 .PHONY: airflow.rm
 airflow.rm: $(AIRFLOW_INIT_CHECK_SENTINEL) ## Remove docker images. Useful fordebugging
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) rm
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) rm
 
 .PHONY: airflow.venv
 airflow.venv: $(AIRFLOW_REQUIREMENTS_TXT) $(AIRFLOW_REQUIREMENTS_EXTRA_TXT) $(AIRFLOW_REQUIREMENTS_TEST_TXT) ## Create a virtual environment folder for Code-completion and tests inside your IDE
-    virtualenv -p python3 $(AIRFLOW_VIRTUAL_ENV_FOLDER); \
-    source $(AIRFLOW_VIRTUAL_ENV_FOLDER)/bin/activate; \
-    export AIRFLOW_GPL_UNIDECODE="yes"; \
-    pip install -r $(AIRFLOW_REQUIREMENTS_TXT); \
-    pip install -r $(AIRFLOW_REQUIREMENTS_EXTRA_TXT);
+	virtualenv -p python3 $(AIRFLOW_VIRTUAL_ENV_FOLDER); \
+	source $(AIRFLOW_VIRTUAL_ENV_FOLDER)/bin/activate; \
+	export AIRFLOW_GPL_UNIDECODE="yes"; \
+	pip install -r $(AIRFLOW_REQUIREMENTS_TXT); \
+	pip install -r $(AIRFLOW_REQUIREMENTS_EXTRA_TXT);
 
 .PHONY: airflow.pip_install
 airflow.pip_install: $(AIRFLOW_REQUIREMENTS_TXT) ## Run pip install -r requirements.txt. This is helpful in a CI environment, where we don't use Docker.
-    SLUGIFY_USES_TEXT_UNIDECODE=yes pip install -r $(AIRFLOW_REQUIREMENTS_TXT)
+	SLUGIFY_USES_TEXT_UNIDECODE=yes pip install -r $(AIRFLOW_REQUIREMENTS_TXT)
 
 .PHONY: airflow.init
 airflow.init: .env ## Initialise Airflow in project
-    $(if $(value GOOGLE_DEFAULT_PROJECT),, $(error GOOGLE_DEFAULT_PROJECT environment variable is not set.))
-    $(info Downloading requirements.txt for version $(AIRFLOW_VERSION) of Airflow)
+	$(if $(value GOOGLE_DEFAULT_PROJECT),, $(error GOOGLE_DEFAULT_PROJECT environment variable is not set.))
+	$(info Downloading requirements.txt for version $(AIRFLOW_VERSION) of Airflow)
 
-    curl --fail -s \
-    "https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/requirements.$(AIRFLOW_VERSION).txt" \
-    -o requirements.txt;
-    # Add the extra requirements
-    touch requirements.extra.txt
+	curl --fail -s \
+	"https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/requirements.$(AIRFLOW_VERSION).txt" \
+	-o requirements.txt;
+	# Add the extra requirements
+	touch requirements.extra.txt
 
-    curl --fail -s \
-    "https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/docker-compose.yml" \
-    -o docker-compose.yml;
+	curl --fail -s \
+	"https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/docker-compose.yml" \
+	-o docker-compose.yml;
 
-    curl --fail -s \
-    "https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/docker-environment-variables.properties" \
-    -o docker-environment-variables.properties;
+	curl --fail -s \
+	"https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/docker-environment-variables.properties" \
+	-o docker-environment-variables.properties;
 
-    curl --fail -s \
-    "https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/Dockerfile" \
-    -o Dockerfile;
+	curl --fail -s \
+	"https://raw.githubusercontent.com/$(BROKKR_REPO)/$(BROKKR_AIRFLOW_PLUGIN_VERSION)/plugins/airflow/docker/Dockerfile" \
+	-o Dockerfile;
 
-    echo "AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT=google-cloud-platform://?extra__google_cloud_platform__project=${GOOGLE_DEFAULT_PROJECT}" >> docker-environment-variables.properties
-    echo "AIRFLOW_CONN_BIGQUERY_DEFAULT=google-cloud-platform://?extra__google_cloud_platform__project=${GOOGLE_DEFAULT_PROJECT}" >> docker-environment-variables.properties
-    echo "Done init of Airflow"
-    echo "Edit personal settings in .env file!"
+	echo "AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT=google-cloud-platform://?extra__google_cloud_platform__project=${GOOGLE_DEFAULT_PROJECT}" >> docker-environment-variables.properties
+	echo "AIRFLOW_CONN_BIGQUERY_DEFAULT=google-cloud-platform://?extra__google_cloud_platform__project=${GOOGLE_DEFAULT_PROJECT}" >> docker-environment-variables.properties
+	echo "Done init of Airflow"
+	echo "Edit personal settings in .env file!"
 
 .env:
-    echo "Creating .env file"
-    echo "AIRFLOW_WEBSERVER_PORT=8080" >> .env
-    echo "DAGS_FOLDER=dags" >> .env
-    echo "TESTS_FOLDER=tests" >> .env
-    echo "AIRFLOW_VARIABLES=$(AIRFLOW_VARIABLES_JSON)" >> .env
+	echo "Creating .env file"
+	echo "AIRFLOW_WEBSERVER_PORT=8080" >> .env
+	echo "DAGS_FOLDER=dags" >> .env
+	echo "TESTS_FOLDER=tests" >> .env
+	echo "AIRFLOW_VARIABLES=$(AIRFLOW_VARIABLES_JSON)" >> .env
 
 
 ############################################################
@@ -117,21 +117,21 @@ airflow.init: .env ## Initialise Airflow in project
 # Check that we have created the needed files for running Airflow
 $(AIRFLOW_INIT_CHECK_SENTINEL):
 ifeq (,$(wildcard $(AIRFLOW_DOCKER_COMPOSE_FILE)))
-    $(error Could not find $(AIRFLOW_DOCKER_COMPOSE_FILE). Maybe you should run make airflow.init?)
+	$(error Could not find $(AIRFLOW_DOCKER_COMPOSE_FILE). Maybe you should run make airflow.init?)
 endif
 ifeq (,$(wildcard Dockerfile))
-    $(error Could not find Dockerfile. Maybe you should run make airflow.init?)
+	$(error Could not find Dockerfile. Maybe you should run make airflow.init?)
 endif
 ifeq (,$(wildcard $(AIRFLOW_VARIABLES_JSON)))
-    $(error Could not find $(AIRFLOW_VARIABLES_JSON). Maybe you should run make airflow.init?)
+	$(error Could not find $(AIRFLOW_VARIABLES_JSON). Maybe you should run make airflow.init?)
 endif
 ifeq (,$(wildcard $(AIRFLOW_REQUIREMENTS_TXT)))
-    $(error Could not find $(AIRFLOW_REQUIREMENTS_TXT). Maybe you should run make airflow.init?)
+	$(error Could not find $(AIRFLOW_REQUIREMENTS_TXT). Maybe you should run make airflow.init?)
 endif
 ifeq (,$(wildcard $(AIRFLOW_REQUIREMENTS_EXTRA_TXT)))
-    $(error Could not find $(AIRFLOW_REQUIREMENTS_EXTRA_TXT). Maybe you should run make airflow.init?)
+	$(error Could not find $(AIRFLOW_REQUIREMENTS_EXTRA_TXT). Maybe you should run make airflow.init?)
 endif
-    touch $@
+	touch $@
 
 # Import Airflow variables json file
 $(AIRFLOW_VARIABLES_SENTINEL): $(AIRFLOW_DB_INIT_SENTINEL) $(AIRFLOW_VARIABLES_JSON)
@@ -162,6 +162,6 @@ $(AIRFLOW_DB_INIT_SENTINEL): $(AIRFLOW_INIT_CHECK_SENTINEL)
     touch $@
 
 $(AIRFLOW_BUILD_SENTINEL): $(AIRFLOW_REQUIREMENTS_EXTRA_TXT) $(AIRFLOW_REQUIREMENTS_TXT)
-    mkdir -p $(AIRFLOW_WORKFOLDER)
-    docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) build
-    touch $@
+	mkdir -p $(AIRFLOW_WORKFOLDER)
+	docker-compose -f $(AIRFLOW_DOCKER_COMPOSE_FILE) build
+	touch $@
